3e85544008ddc4774e1bc7a604bf62a782d763f6
Steve Thompson <writetome51@gmail.com>
2019-04-11 14:18:22 -0600

    Updated dependencies. Upped version to 1.0.10

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var errorIfNotNumber_1 = require("basic-data-handling/errorIfNotNumber");
-var isInteger_isFloat_1 = require("basic-data-handling/isInteger_isFloat");
+var error_if_not_finite_number_1 = require("error-if-not-finite-number");
+var is_integer_is_float_1 = require("@writetome51/is-integer-is-float");
 var get_left_of_decimal_1 = require("@writetome51/get-left-of-decimal");
 // Rounds num using the method taught in school.
 function getRounded(num) {
-    errorIfNotNumber_1.errorIfNotNumber(num);
+    error_if_not_finite_number_1.errorIfNotFiniteNumber(num);
     return Math.round(num) + 0; // the plus-zero fixes strange -0 bug.
 }
 exports.getRounded = getRounded;
 function getRoundedDown(num) {
-    errorIfNotNumber_1.errorIfNotNumber(num);
-    if (isInteger_isFloat_1.isFloat(num))
+    error_if_not_finite_number_1.errorIfNotFiniteNumber(num);
+    if (is_integer_is_float_1.isFloat(num))
         return num < 0 ? get_left_of_decimal_1.getLeftOfDecimal(num - 1) : get_left_of_decimal_1.getLeftOfDecimal(num);
     else
         return num;
 }
 exports.getRoundedDown = getRoundedDown;
 function getRoundedUp(num) {
-    errorIfNotNumber_1.errorIfNotNumber(num);
-    if (isInteger_isFloat_1.isFloat(num))
+    error_if_not_finite_number_1.errorIfNotFiniteNumber(num);
+    if (is_integer_is_float_1.isFloat(num))
         return (getRoundedDown(num) + 1);
     else
         return num;
--- lib/index.ts
+++ lib/index.ts
@@ -1,24 +1,24 @@
-import { errorIfNotNumber } from 'basic-data-handling/errorIfNotNumber';
-import { isFloat } from 'basic-data-handling/isInteger_isFloat';
+import { errorIfNotFiniteNumber } from 'error-if-not-finite-number';
+import { isFloat } from '@writetome51/is-integer-is-float';
 import { getLeftOfDecimal } from '@writetome51/get-left-of-decimal';
 
 
 // Rounds num using the method taught in school.
 export function getRounded(num) {
-	errorIfNotNumber(num);
+	errorIfNotFiniteNumber(num);
 	return Math.round(num) + 0; // the plus-zero fixes strange -0 bug.
 }
 
 
 export function getRoundedDown(num) {
-	errorIfNotNumber(num);
+	errorIfNotFiniteNumber(num);
 	if (isFloat(num)) return num < 0 ? getLeftOfDecimal(num - 1) : getLeftOfDecimal(num);
 	else return num;
 }
 
 
 export function getRoundedUp(num) {
-	errorIfNotNumber(num);
+	errorIfNotFiniteNumber(num);
 	if (isFloat(num)) return (getRoundedDown(num) + 1);
 	else return num;
 }
